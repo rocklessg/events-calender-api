c2c46a91402158ec30adcd476c704e8610c17a05
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-02-17 18:21:14 +0100

    Complete project, preparing for CICD

---
+++ CalendarService.Infrastructure/Data/DbContexts/CalendarServiceDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CalendarService.Core.Entities;
+using CalendarService.Core.Models.Constants;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CalendarService.Infrastructure.Data.DbContexts
+{
+    public class CalendarServiceDbContext : DbContext
+    {
+        public CalendarServiceDbContext()
+        {
+        }
+
+        public CalendarServiceDbContext(DbContextOptions<CalendarServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Event> Event { get; set; }
+        public DbSet<EventMembers> EventMembers { get; set; }
+
+    }
+}
---
+++ CalendarService.Infrastructure/Migrations/20220206230855_InitialDb.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CalendarService.Infrastructure.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EventOrganizer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventMembers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EventId = table.Column<long>(type: "bigint", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventMembers_Event_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventMembers_EventId",
+                table: "EventMembers",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventMembers");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
